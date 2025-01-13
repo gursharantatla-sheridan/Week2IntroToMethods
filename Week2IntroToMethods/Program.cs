13ff8fd0f8b855d172da703eb0174a7138500f7a
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-13 12:42:50 -0500

    pass by value and ref

--- Week2IntroToMethods/Program.cs
+++ Week2IntroToMethods/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num = 5;
+            string str = "hello";
+            int[] arr = { 3, 4, 5 };
+
+            Console.WriteLine("Before changes:\n");
+            Console.WriteLine("num = " + num);
+            Console.WriteLine("str = " + str);
+            Console.WriteLine("arr[0] = " + arr[0]);
+
+            ChangeValue(ref num, str, arr);
+
+            Console.WriteLine("\n\nAfter changes:\n");
+            Console.WriteLine("num = " + num);
+            Console.WriteLine("str = " + str);
+            Console.WriteLine("arr[0] = " + arr[0]);
+        }
+
+        static void ChangeValue(ref int n, string s, int[] a)
+        {
+            n = 100;
+            s = "bye";
+            a[0] = 100;
         }
     }
 }
