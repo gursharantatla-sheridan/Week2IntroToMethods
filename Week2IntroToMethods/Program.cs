b4a0f001290ba39def788ab276aeac966e7d70eb
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-13 13:01:17 -0500

    output parameters

--- Week2IntroToMethods/Program.cs
+++ Week2IntroToMethods/Program.cs
@@ -19,7 +19,38 @@
             Console.WriteLine("num = " + num);
             Console.WriteLine("str = " + str);
             Console.WriteLine("arr[0] = " + arr[0]);
+
+
+
+            // output parameters
+            int x = 10, y = 20;
+            //int sum, mul;
+
+            Calculate(x, y, out int sum, out int mul);
+
+            Console.WriteLine("\n\nSum = " + sum);
+            Console.WriteLine("Mul = " + mul);
+
+
+
+            Console.Write("\n\nEnter a number: ");
+            if (int.TryParse(Console.ReadLine(), out int n))
+                Console.WriteLine("n = " + n);
+            else
+                Console.WriteLine("Invalid input. Please try again");
+
         }
+
+
+
+
+        static void Calculate(int a, int b, out int sum, out int mul)
+        {
+            sum = a + b;
+            mul = a * b;
+        }
+
+
 
         static void ChangeValue(ref int n, string s, int[] a)
         {
